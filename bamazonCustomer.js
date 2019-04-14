abca87e0c419e90cf795405d944ea9b3f04937fd
Chino <ronaldcubillo92@gmail.com>
2019-04-13 20:04:17 -0500

    reading the DB

---
+++ bamazonCustomer.js
@@ -0,0 +1,30 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Chino2015",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+   afterConnection();
+});
+
+function afterConnection() {
+  connection.query("SELECT * FROM products", function(err, res) {
+      for (var i = 0; i < res.length; i++) {
+      console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].deparment_name + " | " + res[i].price);
+    }
+    console.log("-----------------------------------");
+  });
+}
